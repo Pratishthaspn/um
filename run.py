49c288c55b8e372810a43c1deac76b47ba4414e3
Pratty <tpratishtha12@gmail.com>
2026-09-18 19:48:26 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,9 +3,4 @@ from factory import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
     app.run(debug=True)
