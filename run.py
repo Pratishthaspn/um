e405e2ba0995867439ef71f7f4b42b554da54a64
Pratty <tpratishtha12@gmail.com>
2026-09-18 19:41:23 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from app import create_app
+from factory import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)from app import create_app
 
 app = create_app()
 
